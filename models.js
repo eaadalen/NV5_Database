0a227ad692d741d32f3c1a0e9c7f9b3a4abd51c9
eaadalen <eaadalen112@gmail.com>
2024-02-21 12:39:47 -0600

    Added users

--- models.js
+++ models.js
@@ -6,6 +6,24 @@ let projectSchema = mongoose.Schema({
   Description: {type: String, required: true},
 });
 
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
+
 let Project = mongoose.model('Project', projectSchema);
+let User = mongoose.model('User', userSchema);
 
 module.exports.Project = Project;
+module.exports.User = User;
